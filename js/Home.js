29c08eb198d842ac765b83ac2b9fe71b9fb97d1b
Johnatan Gabriel <132827081+JohnatannG@users.noreply.github.com>
2025-02-05 19:22:00 -0300

    Form Receitas

--- js/Home.js
+++ js/Home.js
@@ -1,9 +1,13 @@
 const registrationFormData = JSON.parse(localStorage.getItem("registrationFormData"));
 const userEmail = registrationFormData.emailUser;
 let Despesas = JSON.parse(localStorage.getItem(`FormDespesas_${userEmail}`)) || [];
+let Receitas = JSON.parse(localStorage.getItem(`FormReceitas_${userEmail}`)) || [];
 
 if (!Array.isArray(Despesas)) {
     Despesas = []; 
+}
+if (!Array.isArray(Receitas)) {
+    Receitas = []; 
 }
 
 /*MOSTRAR SALDO NO CARD*/
@@ -20,7 +24,7 @@ LogOut.addEventListener('click', function(){
     logOutModal.classList.add('animate-fade')
 });
 const modalSair = document.getElementById('modalSair').addEventListener('click', function(){
-    window.location.href = 'http://127.0.0.1:5500/pages/Login.html';
+    window.location.href = 'https://app-finance-flame.vercel.app/pages/Login.html';
 })
 const modalCancelar = document.getElementById('modalCancelar').addEventListener('click', function(){
     logOutModal.classList.remove('d-flex');
@@ -42,7 +46,7 @@ fecharModalDespesas.addEventListener('click', function(){
     formAdicionarDespesas.classList.add('d-none');
 });
 
-/*BUTTON FECHAR MODAL ADICIONAR DESPESAS*/
+/*MODAL ADICIONAR DESPESAS*/
 const modalFormDespesas = document.getElementById('modalFormDespesas');
 modalFormDespesas.addEventListener('submit', function(evento){
     evento.preventDefault();
@@ -82,4 +86,54 @@ window.addEventListener('load', () => {
     totalDespesas.textContent = `${valorFormDespesas.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}`
 });
 
+const adicionarReceitas = document.getElementById('adicionarReceitas').addEventListener('click', function(){
+    const formAdicionarReceitas = document.getElementById('formAdicionarReceitas');
+    
+    formAdicionarReceitas.classList.remove('d-none');
+    formAdicionarReceitas.classList.add('d-flex');
+});
 
+const fecharModalReceitas = document.getElementById('fecharModalReceitas').addEventListener('click', function(){
+    const formAdicionarReceitas = document.getElementById('formAdicionarReceitas');
+    
+    formAdicionarReceitas.classList.remove('d-flex');
+    formAdicionarReceitas.classList.add('d-none');
+});
+
+const modalFormReceitas = document.getElementById('modalFormReceitas');
+modalFormReceitas.addEventListener('submit', function(evento){
+    evento.preventDefault();
+
+    const nomeReceitas = document.getElementById('nomeReceitas').value;
+    const valorReceitas = Number(document.getElementById('valorReceitas').value);
+    const dataReceitas = document.getElementById('dataReceitas').value;
+
+    const dadosReceitas = {
+        nomeReceitas: nomeReceitas,
+        valorReceitas: valorReceitas,
+        dataReceitas: dataReceitas
+    }
+
+    Receitas.push(dadosReceitas)
+
+    localStorage.setItem(`FormReceitas_${userEmail}`, JSON.stringify(Receitas));
+
+    const totalReceitas = Receitas.reduce((acc, receita) => acc + receita.valorReceitas, 0);
+    localStorage.setItem(`valorFormReceitas_${userEmail}`, JSON.stringify(totalReceitas));
+
+    modalFormReceitas.reset();
+    mostrarSaldoReceitas()
+});
+
+function mostrarSaldoReceitas(){
+    const valorFormReceitas = JSON.parse(localStorage.getItem(`valorFormReceitas_${userEmail}`));
+    const totalReceitas = document.getElementById('totalReceitas')
+
+    totalReceitas.textContent = `${valorFormReceitas.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}`
+}
+window.addEventListener('load', () => {
+    const valorFormReceitas = JSON.parse(localStorage.getItem(`valorFormReceitas_${userEmail}`));
+    const totalReceitas = document.getElementById('totalReceitas')
+
+    totalReceitas.textContent = `${valorFormReceitas.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}`
+});
